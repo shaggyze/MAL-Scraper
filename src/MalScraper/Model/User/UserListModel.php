28fbddc562644d25d6a7f6db9f06cac00843a515
shaggyze <shaggyze@gmail.com>
2024-07-08 15:22:49 -0700

    Update UserCoverModel.php

--- src/MalScraper/Model/User/UserListModel.php
+++ src/MalScraper/Model/User/UserListModel.php
@@ -104,6 +104,16 @@ class UserListModel extends MainModel
 			  $content2 = json_decode(file_get_contents($url2), true);
 			  $content[$i]['synopsis'] = $content2['data']['synopsis'];
 			}
+			if (!empty($content[$i]['anime_id'])) {
+			  $url2 = 'https://shaggyze.website/msa/info?t=anime&id=' . $content[$i]['anime_id'];
+			  $content2 = json_decode(file_get_contents($url2), true);
+			  $content[$i]['rank'] = $content2['data']['rank'];
+			}
+			if (!empty($content[$i]['manga_id'])) {
+			  $url2 = 'https://shaggyze.website/msa/info?t=manga&id=' . $content[$i]['manga_id'];
+			  $content2 = json_decode(file_get_contents($url2), true);
+			  $content[$i]['rank'] = $content2['data']['rank'];
+			}
 			if (is_array($content[$i]['anime_studios'])) {
 			  $content[$i]['anime_studios'] = implode(", ", $content[$i]['anime_studios']);
 			} else {
