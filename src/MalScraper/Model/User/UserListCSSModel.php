b808cd13d4e7e4a1cad96968a30cb55e354a3a20
shaggyze <shaggyze@gmail.com>
2024-09-24 21:28:06 -0700

    Update UserListCSSModel.php

--- src/MalScraper/Model/User/UserListCSSModel.php
+++ src/MalScraper/Model/User/UserListCSSModel.php
@@ -128,7 +128,7 @@ class UserListCSSModel extends MainModel
 			if (!empty($content2['data']['synopsis'])) {
 			  $synopsis = preg_replace('/[\x0D]/', "", $content2['data']['synopsis']);
 			  $synopsis = str_replace(array('nn', "\n", "\t", "\r"), "", $synopsis);
-			  $synopsis = str_replace(['"', '-'], ' ', $synopsis);
+			  $synopsis = str_replace(['"', '—'], ' ', $synopsis);
 			  $synopsis = str_replace("'", '', $synopsis);
 			  $content[$i]['synopsis'] = $synopsis;
 			} else {
