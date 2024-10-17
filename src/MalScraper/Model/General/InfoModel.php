47afa2eeff647bc8501cf05a9b915cac8e79f630
shaggyze <shaggyze@gmail.com>
2024-10-17 10:37:04 -0700

    Update InfoModel.php

--- src/MalScraper/Model/General/InfoModel.php
+++ src/MalScraper/Model/General/InfoModel.php
@@ -133,7 +133,7 @@ private function getTitle2()
  * @param string                             $type
  *
  * @return string
- */
+
 private function getTitle3($title_info, $type)
 {
     foreach ($title_info as $titleElement) {
@@ -150,7 +150,13 @@ private function getTitle3($title_info, $type)
     }
 
     return 'N/A 2';
-}
+} */
+    private function getTitle3($title_info, $type)
+    {
+        preg_match('/('.$type.':<\/span>)([^<]*)/', $title_info->innertext, $title2);
+
+        return trim($title2 ? $title2[2] : 'N/A 2');
+    }
 
     /**
      * Get anime/manga promotional video.
