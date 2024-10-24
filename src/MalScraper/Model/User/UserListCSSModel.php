8bda32c24ae9d7533a3f7905fa50cc071676e170
shaggyze <shaggyze@gmail.com>
2024-10-24 08:07:40 -0700

    Update UserListCSSModel.php

--- src/MalScraper/Model/User/UserListCSSModel.php
+++ src/MalScraper/Model/User/UserListCSSModel.php
@@ -227,9 +227,13 @@ class UserListCSSModel extends MainModel
 			  $content[$i]['manga_english'] = str_replace(['"', '[', ']'], '', $content[$i]['manga_english']);
 			}
 			if (!empty($content[$i]['num_watched_episodes'])) {
-			  if $content[$i]['anime_num_episodes'] !== 0 {$content[$i]['progress_percent'] = round(($content[$i]['num_watched_episodes'] / $content[$i]['anime_num_episodes']) * 100, 2)}
+			  if $content[$i]['anime_num_episodes'] !== 0 {
+				  $content[$i]['progress_percent'] = round(($content[$i]['num_watched_episodes'] / $content[$i]['anime_num_episodes']) * 100, 2);
+			  }
 			} else {
-			  if $content[$i]['manga_num_volumes'] !== 0 {$content[$i]['progress_percent'] = round(($content[$i]['num_read_volumes'] / $content[$i]['manga_num_volumes']) * 100, 2)}
+			  if $content[$i]['manga_num_volumes'] !== 0 {
+				  $content[$i]['progress_percent'] = round(($content[$i]['num_read_volumes'] / $content[$i]['manga_num_volumes']) * 100, 2);
+			  }
 			}
 			if (!empty($content2['data']['rank'])) {
 			  $content[$i]['rank'] = $content2['data']['rank'];
