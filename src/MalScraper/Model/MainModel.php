52e215089dcfcc39cdec9349cc326a06d6458c78
shaggyze <shaggyze@gmail.com>
2024-11-05 17:34:05 -0800

    Update MainModel.php

--- src/MalScraper/Model/MainModel.php
+++ src/MalScraper/Model/MainModel.php
@@ -60,15 +60,15 @@ class MainModel
         $file_headers = @get_headers($url);
 		$html = HtmlDomParser::file_get_html($url);
 		$title = $html->find('title', 0)->plaintext;
-        if ($file_headers[0] == 'HTTP/1.1 404 Not Found') || $title == '404 Not Found - MyAnimeList.net') {
+        if ($file_headers[0] == 'HTTP/1.1 404 Not Found' || $title == '404 Not Found - MyAnimeList.net') {
             return 404;
         }
 
-        if (empty($file_headers) || $file_headers[0] == 'HTTP/1.1 403 Forbidden') || $title == '403 Forbidden') {
+        if (empty($file_headers) || $file_headers[0] == 'HTTP/1.1 403 Forbidden' || $title == '403 Forbidden') {
             return 403;
         }
 
-        if ($file_headers[0] == 'HTTP/1.1 504 Gateway Time-out') || $title == '504 Gateway Time-out') {
+        if ($file_headers[0] == 'HTTP/1.1 504 Gateway Time-out' || $title == '504 Gateway Time-out') {
             return 504;
         }
 
