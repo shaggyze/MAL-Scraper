807c91d8a568bb78c7fa6820ddb6de12c115abd5
shaggyze <shaggyze@gmail.com>
2024-11-11 02:15:22 -0800

    Update InfoModel.php

--- src/MalScraper/Model/General/InfoModel.php
+++ src/MalScraper/Model/General/InfoModel.php
@@ -605,7 +605,7 @@ class InfoModel extends MainModel
             foreach ($song_area->find('td') as $each_song) {
                 $each_song = trim(preg_replace('/#\d*:\s/', '', $each_song->plaintext));
 				$song_index++;
-                $song['openings'][$song_index]] = $each_song;
+                $song['openings'][$song_index] = $each_song;
             }
         }
 
@@ -615,7 +615,7 @@ class InfoModel extends MainModel
             foreach ($song_area->find('td') as $each_song) {
                 $each_song = trim(preg_replace('/#\d*:\s/', '', $each_song->plaintext));
 				$song_index++;
-                $song['endings'][$song_index]] = $each_song;
+                $song['endings'][$song_index] = $each_song;
             }
         }
 
