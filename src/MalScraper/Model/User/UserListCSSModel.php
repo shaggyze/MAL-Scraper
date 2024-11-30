07236715b6ebb6e06a01c3ffe270ef1c00977dc8
shaggyze <shaggyze@gmail.com>
2024-11-30 15:11:25 -0800

    Update UserListCSSModel.php

--- src/MalScraper/Model/User/UserListCSSModel.php
+++ src/MalScraper/Model/User/UserListCSSModel.php
@@ -230,13 +230,13 @@ class UserListCSSModel extends MainModel
 			  if ($content2['data']['title_german'] !== null) {
 			    $content[$i]['anime_title_de'] = str_replace(['"', '[', ']'], '', $content2['data']['title_german']);
 			  } else {
-			    $content[$i]['anime_title_de'] = 'N/A';
+			    $content[$i]['anime_title_de'] = $content[$i]['anime_title_eng'];
 			  }
 			} else {
 			  if ($content2['data']['title_german'] !== null) {
 			    $content[$i]['manga_title_de'] = str_replace(['"', '[', ']'], '', $content2['data']['title_german']);
 			  } else {
-			    $content[$i]['manga_title_de'] = 'N/A';
+			    $content[$i]['manga_title_de'] = $content[$i]['manga_english'];
 			  }
 			}
 			if (!empty($content[$i]['num_watched_episodes'])) {
