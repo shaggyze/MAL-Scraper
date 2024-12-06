3d9780bfc9c047b9977eaa5b46662b3adf3a9835
shaggyze <shaggyze@gmail.com>
2024-12-06 00:24:11 -0800

    Update UserListCSSModel.php

--- src/MalScraper/Model/User/UserListCSSModel.php
+++ src/MalScraper/Model/User/UserListCSSModel.php
@@ -319,6 +319,7 @@ class UserListCSSModel extends MainModel
 			} else {
 			  $content[$i]['manga_image_path'] = Helper::imageUrlReplace($content[$i]['manga_id'], 'manga', $content[$i]['manga_image_path'], $this->_user);
 			}
+			error_log(trim($content[$i]['status']));
 			switch (trim($content[$i]['status'])) {
 			  case '1':
 			    $te_cwr++;
