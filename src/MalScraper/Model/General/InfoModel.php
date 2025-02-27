48c4f78cc91e7634fa69ac20428c3d8a1d40b71d
shaggyze <shaggyze@gmail.com>
2025-02-26 19:43:13 -0800

    Update InfoModel.php

--- src/MalScraper/Model/General/InfoModel.php
+++ src/MalScraper/Model/General/InfoModel.php
@@ -110,7 +110,7 @@ private function getImages()
     }
 
     // Parse the fetched HTML
-    $doc = str_get_html($html); // Assuming you're using Simple HTML DOM Parser
+    $doc = HtmlDomParser::str_get_html($html); // Assuming you're using Simple HTML DOM Parser
 
     if (!$doc) {
         return 'Failed to parse pictures page.';
