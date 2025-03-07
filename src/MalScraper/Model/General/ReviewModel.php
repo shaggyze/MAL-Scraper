0c6c1df489887cf40546250f8aae250285357d78
shaggyze <shaggyze@gmail.com>
2025-03-07 04:03:45 -0800

    Update ReviewModel.php

--- src/MalScraper/Model/General/ReviewModel.php
+++ src/MalScraper/Model/General/ReviewModel.php
@@ -286,7 +286,7 @@ class ReviewModel extends MainModel
         $data = [];
         $review_area = $this->_parser->find('.review-element', 0);
 
-        $top_area = $review_area->find('.body', 0);
+        $top_area = $review_area->find('.titleblock', 0);
         $bottom_area = $top_area->next_sibling();
 
         $data['id'] = $this->getId();
