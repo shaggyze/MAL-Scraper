98f7301f1f3612ac8eddccf2b987abb1f7fef09f
shaggyze <shaggyze@gmail.com>
2025-06-27 03:03:23 -0700

    Update ProducerModel.php

--- src/MalScraper/Model/General/ProducerModel.php
+++ src/MalScraper/Model/General/ProducerModel.php
@@ -23,7 +23,6 @@ class ProducerModel extends MainModel
     private static $mangaGenreMap = [
         "1" => "Action", "2" => "Adventure", "5" => "Avant Garde", "46" => "Award Winning", "28" => "Boys Love", "4" => "Comedy", "8" => "Drama", "10" => "Fantasy", "26" => "Girls Love", "47" => "Gourmet", "14" => "Horror", "7" => "Mystery", "22" => "Romance", "24" => "Sci-Fi", "36" => "Slice of Life", "30" => "Sports", "37" => "Supernatural", "45" => "Suspense", "9" => "Ecchi", "49" => "Erotica", "12" => "Hentai", "50" => "Adult Cast", "51" => "Anthropomorphic", "52" => "CGDCT", "53" => "Childcare", "54" => "Combat Sports", "44" => "Crossdressing", "55" => "Delinquents", "39" => "Detective", "56" => "Educational", "57" => "Gag Humor", "58" => "Gore", "35" => "Harem", "59" => "High Stakes Game", "13" => "Historical", "60" => "Idols (Female)", "61" => "Idols (Male)", "62" => "Isekai", "63" => "Iyashikei", "64" => "Love Polygon", "75" => "Love Status Quo", "65" => "Magical Sex Shift", "66" => "Mahou Shoujo", "17" => "Martial Arts", "18" => "Mecha", "67" => "Medical", "68" => "Memoir", "38" => "Military", "19" => "Music", "6" => "Mythology", "69" => "Organized Crime", "70" => "Otaku Culture", "20" => "Parody", "71" => "Performing Arts", "72" => "Pets", "40" => "Psychological", "3" => "Racing", "73" => "Reincarnation", "74" => "Reverse Harem", "21" => "Samurai", "23" => "School", "76" => "Showbiz", "29" => "Space", "11" => "Strategy Game", "31" => "Super Power", "77" => "Survival", "78" => "Team Sports", "79" => "Time Travel", "83" => "Urban Fantasy", "32" => "Vampire", "80" => "Video Game", "81" => "Villainess", "82" => "Visual Arts", "48" => "Workplace", "42" => "Josei", "15" => "Kids", "41" => "Seinen", "25" => "Shoujo", "27" => "Shounen"
     ];
-    // --- END: GENRE MAPPINGS ---
 
     public function __construct($type, $type2, $id, $page = 1, $parserArea = '#content')
     {
@@ -61,8 +60,7 @@ class ProducerModel extends MainModel
         if ($imageDiv && is_object($imageDiv)) {
             $imgTag = $imageDiv->find('img', 0);
             if ($imgTag && is_object($imgTag)) {
-                if ($imgTag->hasAttribute('data-src')) return Helper::imageUrlCleaner($imgTag->getAttribute('data-src'));
-                if ($imgTag->hasAttribute('src')) return Helper::imageUrlCleaner($imgTag->getAttribute('src'));
+                return Helper::imageUrlCleaner($imgTag->getAttribute('data-src') ?: $imgTag->getAttribute('src'));
             }
         }
         return '';
@@ -71,12 +69,9 @@ class ProducerModel extends MainModel
     private function getAnimeId($name_area)
     {
         if (!$name_area || !is_object($name_area)) return '';
-        // For anime cards: div.title > a
-        // For manga cards: div.title > div.title-text > h2 > a
         $linkNode = $name_area->find('a', 0); 
         if ($linkNode && is_object($linkNode) && isset($linkNode->href)) {
             $id_parts = explode('/', $linkNode->href);
-            // Universal for /anime/ID/... and /manga/ID/...
             if (isset($id_parts[3]) && in_array($id_parts[3], ['anime', 'manga']) && isset($id_parts[4]) && is_numeric($id_parts[4])) {
                 return $id_parts[4];
             }
@@ -112,7 +107,7 @@ class ProducerModel extends MainModel
 
     private function _getStudioPageDetails()
     {
-        if (!$this->_parser || !is_object($this->_parser)) return ['error_studio_details' => 'Parser not available.'];
+        if (!$this->_parser || !is_object($this->_parser)) return [];
         $studioPageDetails = ['studio_name' => '','logo_image_url' => '','info' => [],'description' => '','available_at_links' => [],'resource_links' => []];
         $studioNameNode = $this->_parser->find('h1.title-name', 0);
         if ($studioNameNode && is_object($studioNameNode) && isset($studioNameNode->plaintext)) {
@@ -194,9 +189,9 @@ class ProducerModel extends MainModel
     {
         $producer = [];
         if (!is_object($each_anime)) return $producer;
-        $properties_node = $each_anime->find('div.properties', 0); // Simplified from div.synopsis div.properties
+        $properties_node = $each_anime->find('div.properties', 0);
         if ($properties_node && is_object($properties_node)) {
-            $targetCaption = ($this->_type == 'anime') ? 'Studio' : 'Author'; // Manga uses "Author"
+            $targetCaption = ($this->_type == 'anime') ? 'Studio' : 'Author';
             foreach ($properties_node->find('div.property') as $propDiv) {
                 if(!is_object($propDiv)) continue;
                 $captionNode = $propDiv->find('span.caption', 0);
@@ -218,18 +213,13 @@ class ProducerModel extends MainModel
     private function getAnimeEpisode($each_anime)
     {
         if (!is_object($each_anime)) return '';
-        // For manga: <span class="item"><span class="volume js-volume">24</span> vol, <span class="chapter js-chapter">96</span> chp</span>
-        if ($this->_type == 'manga') {
-            $info_node = $each_anime->find('div.prodsrc div.info span.item', 2); // Third span.item
-            if ($info_node && is_object($info_node)) {
-                $volumeNode = $info_node->find('span.volume', 0);
-                if ($volumeNode && is_object($volumeNode) && isset($volumeNode->plaintext)) return trim($volumeNode->plaintext);
-            }
-        } else { // For anime
-            $prodsrc_info_node = $each_anime->find('div.prodsrc div.info', 0);
-            if ($prodsrc_info_node && is_object($prodsrc_info_node)) {
-                $spans = $prodsrc_info_node->find('span.item');
-                foreach ($spans as $span) {
+        $prodsrc_info_node = $each_anime->find('div.prodsrc div.info', 0);
+        if ($prodsrc_info_node && is_object($prodsrc_info_node)) {
+            if ($this->_type == 'manga') {
+                $volSpan = $prodsrc_info_node->find('span.volume',0);
+                if ($volSpan && is_object($volSpan) && isset($volSpan->plaintext)) return trim($volSpan->plaintext);
+            } else {
+                foreach ($prodsrc_info_node->find('span.item') as $span) {
                     if (is_object($span) && isset($span->plaintext) && strpos($span->plaintext, 'ep') !== false) {
                         if (preg_match('/(\d+)\s*ep/', $span->plaintext, $matches)) return $matches[1];
                     }
@@ -242,9 +232,9 @@ class ProducerModel extends MainModel
     private function getAnimeSource($each_anime)
     {
         if (!is_object($each_anime)) return '';
-        $properties_node = $each_anime->find('div.properties', 0); // Simplified
+        $properties_node = $each_anime->find('div.properties', 0);
         if ($properties_node && is_object($properties_node)) {
-            $targetCaption = 'Source'; // Always "Source" for anime
+            $targetCaption = 'Source';
             foreach ($properties_node->find('div.property') as $propDiv) {
                 if(!is_object($propDiv)) continue;
                 $captionNode = $propDiv->find('span.caption', 0);
@@ -301,7 +291,7 @@ class ProducerModel extends MainModel
     {
         if (!is_object($each_anime)) return ($this->_type == 'anime') ? [] : '';
         if ($this->_type == 'anime') {
-            $synopsisNode = $each_anime->find('div.synopsis.js-synopsis', 0);
+            $synopsisNode = $each_anime->find('div.synopsis.js-synopsis', 0); 
             if ($synopsisNode && is_object($synopsisNode)) {
                 $licensorNode = $synopsisNode->find('span.licensors', 0); 
                 if ($licensorNode && is_object($licensorNode) && $licensorNode->hasAttribute('data-licensors')) {
@@ -310,13 +300,13 @@ class ProducerModel extends MainModel
             }
             return [];
         } else { // Manga: Find Serialization
-            $properties_node = $each_anime->find('div.properties', 0); // Serialization is in properties
+            $properties_node = $each_anime->find('div.properties', 0);
             if ($properties_node && is_object($properties_node)) {
                 foreach ($properties_node->find('div.property') as $propDiv) {
                     if(!is_object($propDiv)) continue;
                     $captionNode = $propDiv->find('span.caption', 0);
                     if ($captionNode && is_object($captionNode) && isset($captionNode->plaintext) && trim($captionNode->plaintext) == 'Serialization') {
-                        $itemNode = $propDiv->find('span.item a', 0); // Usually a link
+                        $itemNode = $propDiv->find('span.item a', 0);
                         if ($itemNode && is_object($itemNode) && isset($itemNode->plaintext)) return trim($itemNode->plaintext);
                         break;
                     }
@@ -335,7 +325,7 @@ class ProducerModel extends MainModel
             if (isset($itemSpans[0]) && is_object($itemSpans[0]) && isset($itemSpans[0]->plaintext)) {
                 $full_text = $itemSpans[0]->plaintext;
                 $parts = explode(',', $full_text);
-                return trim($parts[0]); // e.g., "Movie" or "Manga"
+                return trim($parts[0]);
             }
         }
         return '';
@@ -344,16 +334,14 @@ class ProducerModel extends MainModel
     private function getAnimeStart($each_anime)
     {
         if (!is_object($each_anime)) return '';
-        $jsStartDate = $each_anime->find('span.js-start_date', 0); // Anime cards
+        $jsStartDate = $each_anime->find('span.js-start_date', 0);
         if ($jsStartDate && is_object($jsStartDate) && isset($jsStartDate->plaintext)) return trim($jsStartDate->plaintext);
-        // For Manga cards: "Manga, 2004"
         $prodsrc_info_node = $each_anime->find('div.prodsrc div.info', 0);
         if ($prodsrc_info_node && is_object($prodsrc_info_node)) {
             $itemSpans = $prodsrc_info_node->find('span.item');
             if (isset($itemSpans[0]) && is_object($itemSpans[0]) && isset($itemSpans[0]->plaintext)) {
-                $full_text = $itemSpans[0]->plaintext; // "Manga, 2004"
-                $parts = explode(',', $full_text);
-                if (isset($parts[1])) return trim($parts[1]); // "2004"
+                $parts = explode(',', $itemSpans[0]->plaintext);
+                if (isset($parts[1])) return trim($parts[1]);
             }
         }
         return '';
@@ -362,12 +350,11 @@ class ProducerModel extends MainModel
     private function getAnimeScore($each_anime)
     {
         if (!is_object($each_anime)) return 'N/A';
-        $jsScore = $each_anime->find('span.js-score', 0); // For Anime cards
+        $jsScore = $each_anime->find('span.js-score', 0);
         if ($jsScore && is_object($jsScore) && isset($jsScore->plaintext)) {
             $score = trim($jsScore->plaintext);
             return (is_numeric($score) && (float)$score >= 0) ? $score : 'N/A';
         }
-        // For Manga cards: div.information div.scormem-item.score
         $infoNode = $each_anime->find('div.information', 0);
         if ($infoNode && is_object($infoNode)) {
              $scoreNode = $infoNode->find('div.scormem-item.score', 0);
@@ -375,7 +362,6 @@ class ProducerModel extends MainModel
                  if(preg_match('/(\d+\.?\d*)/', $scoreNode->plaintext, $matches)) return $matches[1];
              }
         }
-        // Fallback for Anime cards' visible score
         $widget = $each_anime->find('div.widget', 0);
         if ($widget && is_object($widget)) {
             $starsNode = $widget->find('div.stars', 0);
@@ -389,11 +375,10 @@ class ProducerModel extends MainModel
     private function getAnimeMember($each_anime)
     {
         if (!is_object($each_anime)) return '0';
-        $jsMembers = $each_anime->find('span.js-members', 0); // For Anime cards
+        $jsMembers = $each_anime->find('span.js-members', 0);
         if ($jsMembers && is_object($jsMembers) && isset($jsMembers->plaintext)) {
             return trim(preg_replace('/[^\d]/', '', $jsMembers->plaintext));
         }
-        // For Manga cards: div.information div.scormem-item.member
         $infoNode = $each_anime->find('div.information', 0);
         if ($infoNode && is_object($infoNode)) {
             $memberNode = $infoNode->find('div.scormem-item.member', 0);
@@ -408,7 +393,6 @@ class ProducerModel extends MainModel
                 }
             }
         }
-        // Fallback for Anime cards' visible members
         $widget = $each_anime->find('div.widget', 0);
         if ($widget && is_object($widget)) {
             $usersNode = $widget->find('div.users', 0);
@@ -427,7 +411,7 @@ class ProducerModel extends MainModel
         }
 
         $outputData = [];
-        if ($this->_type == 'anime') { // Studio details are only for anime producer pages
+        if ($this->_type == 'anime') {
             $studioPageDetails = $this->_getStudioPageDetails();
             if (is_array($studioPageDetails)) {
                 foreach ($studioPageDetails as $key => $value) {
@@ -435,56 +419,60 @@ class ProducerModel extends MainModel
                     elseif (!empty($value)) $outputData['studio_details_error'] = $value;
                 }
             }
-        } else { // For manga, initialize with empty studio details
-            $outputData = ['studio_name' => '','logo_image_url' => '','info' => [],'description' => '','available_at_links' => [],'resource_links' => []];
+        } else { // For manga, just get the magazine name.
+            $magazineNameNode = $this->_parser->find('div.normal_header span.di-ib', 0);
+            if ($magazineNameNode && is_object($magazineNameNode) && isset($magazineNameNode->plaintext)) {
+                $outputData['magazine_name'] = trim(preg_replace('/\(\d+\)$/', '', $magazineNameNode->plaintext));
+            }
         }
 
-        $animeListData = [];
+        $workListData = [];
         // Use a flexible selector for the list of items
-        $anime_table = $this->_parser->find('div.seasonal-anime.js-seasonal-anime');
+        $work_table = $this->_parser->find('div.seasonal-anime.js-seasonal-anime');
+        if(empty($work_table) && $this->_type == 'anime') { // Fallback to your original anime-specific one if needed
+            $work_table = $this->_parser->find('div[class="js-anime-category-studio seasonal-anime js-seasonal-anime js-anime-type-all js-anime-type-3"]');
+        }
 
-        if (is_array($anime_table)) {
-            foreach ($anime_table as $each_anime) {
-                if(!is_object($each_anime)) continue; 
+        if (is_array($work_table)) {
+            foreach ($work_table as $each_work) {
+                if(!is_object($each_work)) continue; 
                 $result = [];
-                // The title div is structured differently in each card type
-                $name_area = $each_anime->find('div.title', 0);
+                $name_area = $each_work->find('div.title', 0);
                 
                 $result['id'] = ''; $result['title'] = ''; $result['image'] = '';
                 if ($name_area && is_object($name_area)) {
                     $result['id'] = $this->getAnimeId($name_area);
                     $result['title'] = $this->getAnimeTitle($name_area);
                 }
-                $result['image'] = $this->getAnimeImage($each_anime);
+                $result['image'] = $this->getAnimeImage($each_work);
                 if (empty($result['id'])) continue; 
 
-                // --- Rest of the fields ---
-                $result['genre'] = $this->getAnimeGenre($each_anime);
-                $result['synopsis'] = $this->getAnimeSynopsis($each_anime);
-                $result['source'] = $this->getAnimeSource($each_anime);
-
+                $result['genre'] = $this->getAnimeGenre($each_work);
+                $result['synopsis'] = $this->getAnimeSynopsis($each_work);
+                
                 if ($this->_type == 'anime') {
-                    $result['producer'] = $this->getAnimeProducer($each_anime);
-                    $result['episode'] = $this->getAnimeEpisode($each_anime);
-                    $result['licensor'] = $this->getAnimeLicensor($each_anime); 
-                    $result['type'] = $this->getAnimeType($each_anime);
-                } else { // Manga fields
-                    $result['author'] = $this->getAnimeProducer($each_anime); 
-                    $result['volume'] = $this->getAnimeEpisode($each_anime);  
-                    $result['serialization'] = $this->getAnimeLicensor($each_anime); 
+                    $result['source'] = $this->getAnimeSource($each_work);
+                    $result['producer'] = $this->getAnimeProducer($each_work);
+                    $result['episode'] = $this->getAnimeEpisode($each_work);
+                    $result['licensor'] = $this->getAnimeLicensor($each_work); 
+                    $result['type'] = $this->getAnimeType($each_work);
+                } else { 
+                    $result['author'] = $this->getAnimeProducer($each_work); 
+                    $result['volume'] = $this->getAnimeEpisode($each_work);  
+                    $result['serialization'] = $this->getAnimeLicensor($each_work); 
                 }
 
-                $result['airing_start'] = $this->getAnimeStart($each_anime);
-                $result['member'] = $this->getAnimeMember($each_anime);
-                $result['score'] = $this->getAnimeScore($each_anime);   
+                $result['airing_start'] = $this->getAnimeStart($each_work);
+                $result['member'] = $this->getAnimeMember($each_work);
+                $result['score'] = $this->getAnimeScore($each_work);   
                 
-                $animeListData[] = $result;
+                $workListData[] = $result;
             }
         }
 
         // Merge the anime/manga list into the output
-        foreach ($animeListData as $index => $animeItem) {
-            $outputData[(string)$index] = $animeItem;
+        foreach ($workListData as $index => $workItem) {
+            $outputData[(string)$index] = $workItem;
         }
         
         return $outputData;
