4aeaf781eb6475abb294b04115cb89dfce32fd71
shaggyze <shaggyze@gmail.com>
2025-07-14 04:59:24 -0700

    Update UserListCSSModel.php

--- src/MalScraper/Model/User/UserListCSSModel.php
+++ src/MalScraper/Model/User/UserListCSSModel.php
@@ -77,6 +77,8 @@ class UserListCSSModel extends MainModel
         return call_user_func_array([$this, $method], $arguments);
     }
 
+
+
     /**
      * Get user list.
      *
@@ -94,50 +96,13 @@ class UserListCSSModel extends MainModel
 	  $te_ptwr = 0;
 
 	  while (true) {
-        $primary_url = $this->_myAnimeListUrl.'/'.$this->_type.'list/'.$this->_user.'/load.json?offset='.$offset.'&status='.$this->_status.'&genre='.$this->_genre;
+		$url = $this->_myAnimeListUrl.'/'.$this->_type.'list/'.$this->_user.'/load.json?offset='.$offset.'&status='.$this->_status.'&genre='.$this->_genre;
 
-        $content_json = false;
-        $http_status = null;
-        $use_alternate_url = false;
+		$content = json_decode(file_get_contents(htmlspecialchars_decode($url)), true);
 
-        $context = stream_context_create([
-            'http' => [ 'ignore_errors' => true ]
-        ]);
-
-        $content_json = @file_get_contents(htmlspecialchars_decode($primary_url), false, $context);
-
-        if (isset($http_response_header) && count($http_response_header) > 0) {
-            preg_match('{HTTP\/\S+\s(\d{3})}', $http_response_header[0], $match);
-            if (isset($match[1])) {
-                $http_status = (int)$match[1];
-            }
-        }
-
-        if ($content_json === false || ($http_status === 405)) {
-            $use_alternate_url = true;
-            echo "DEBUG: Primary URL failed. Attempting alternate URL.\n";
-        }
-
-        if ($use_alternate_url) {
-            $alternate_url = 'https://shaggyze.website/maldb/userlist/'.$this->_user.'_'.$this->_type.'_'.$this->_status.'_'.$this->_genre.'.json';
-            echo "DEBUG: Using alternate URL: " . $alternate_url . "\n";
-            $content_json = @file_get_contents(htmlspecialchars_decode($alternate_url));
-        }
-
-        $content = null;
-        if ($content_json) {
-            $content = json_decode($content_json, true);
-        }
-
-        // FIX #1: If the alternate URL was used, get the actual data from the 'data' wrapper.
-        if ($use_alternate_url && isset($content['data']) && is_array($content['data'])) {
-            $content = array_values($content['data']);
-        }
-		
 		if ($content) {
 		  $count = count($content);
 		  for ($i = 0; $i < $count; $i++) {
-			// --- ALL OF YOUR ORIGINAL PROCESSING LOGIC REMAINS UNCHANGED ---
 			if (!empty($content[$i]['anime_id'])) {
 			  $subdirectory = get_subdirectory('info', 'anime', $content[$i]['anime_id']);
 			  $url1 = 'https://shaggyze.website/msa/info?t=anime&id=' . $content[$i]['anime_id'];
@@ -153,31 +118,249 @@ class UserListCSSModel extends MainModel
 			  $content2 = json_decode(file_get_contents(htmlspecialchars_decode($url2)), true);
 			  if ($content[$i]['manga_english'] == "") {$content[$i]['manga_english'] = "N/A";}
 			}
-			// ... all of your other logic here ...
-			if ($content[$i]['status'] == 1) { $te_cwr++; $te_all++; } 
-            elseif ($content[$i]['status'] == 2) { $te_c++; $te_all++; } 
-            elseif ($content[$i]['status'] == 3) { $te_oh++; $te_all++; } 
-            elseif ($content[$i]['status'] == 4) { $te_d++; $te_all++; } 
-            elseif ($content[$i]['status'] == 6) { $te_ptwr++; $te_all++; }
-            // ... etc. ...
+			if (!empty($content2['data']['broadcast'])) {
+				$content[$i]['broadcast'] = $content2['data']['broadcast'];
+			} else {
+				$content[$i]['broadcast'] = "";
+			}
+			if (!empty($content2['data']['synopsis'])) {
+			  $synopsis = preg_replace('/[\x0D]/', "", $content2['data']['synopsis']);
+			  $synopsis = str_replace(array("\n", "\t", "\r"), "-a ", $synopsis);
+			  $synopsis = str_replace('"', '-"', $synopsis);
+			  $synopsis = str_replace("'", "-'", $synopsis);
+			  $content[$i]['synopsis'] = $synopsis;
+			} else {
+			  $content[$i]['synopsis'] = "N/A";
+			}
+			if (!empty($content2['data']['duration'])) {
+			  $episodes = intval($content2['data']['episodes']);
+			  $duration = intval(str_replace(' min. per ep.', '', $content2['data']['duration']));
+			  if ($episodes > 0 && $duration > 0) {
+			    $content[$i]['total_runtime'] = floor($episodes * $duration / 60) . 'h ' . ($episodes * $duration % 60) . 'm';
+			  } else {
+				$content[$i]['total_runtime'] = 'N/A';
+			  }
+			} else {
+				$content[$i]['total_runtime'] = 'N/A';
+			}
+			if (!empty($content2['data']['premiered'])) {
+			  $content[$i]['year'] = str_replace(['Winter ', 'Spring ', 'Summer ', 'Fall '], '', $content2['data']['premiered']);
+			} else {
+			  if (!empty($content2['data']['aired']['start'])) {
+			    $content[$i]['year'] = (int) substr($content2['data']['aired']['start'], -4);
+			  } else {
+			    if (!empty($content2['data']['published']['start'])) {
+			      $content[$i]['year'] = (int) substr($content2['data']['published']['start'], -4);
+			    } else {
+			      $content[$i]['year'] = 'N/A';
+			    }
+			  }
+			}
+			if (!empty($content2['data']['genres'])) {
+			  $genres = $content2['data']['genres'];
+			  $genreNames = '';
+			  if (is_array($genres)) {
+			    foreach ($genres as $genre) {
+				  $genreNames .= $genre['name'] . ', ';
+			    }
+			  }
+			  $content[$i]['genres'] = rtrim($genreNames, ', ');
+			} else {
+			  if (!empty($content2['data']['genre'])) {
+			    $genres = $content2['data']['genre'];
+			    $genreNames = '';
+				if (is_array($genres)) {
+			      foreach ($genres as $genre) {
+				    $genreNames .= $genre['name'] . ', ';
+			      }
+				}
+			    $content[$i]['genres'] = rtrim($genreNames, ', ');
+			  } else {
+			  $content[$i]['genres'] = 'N/A';
+			  }
+			}
+			if (!empty($content2['data']['themes'])) {
+			  $themes = $content2['data']['themes'];
+			  $themeNames = '';
+			  if (is_array($themes)) {
+			    foreach ($themes as $theme) {
+				  $themeNames .= $theme['name'] . ', ';
+			    }
+			  }
+			  $content[$i]['themes'] = rtrim($themeNames, ', ');
+			} else {
+			  if (!empty($content2['data']['theme'])) {
+			    $themes = $content2['data']['theme'];
+			    $themeNames = '';
+			    if (is_array($themes)) {
+			    foreach ($themes as $theme) {
+				  $themeNames .= $theme['name'] . ', ';
+			    }
+			    }
+			    $content[$i]['themes'] = rtrim($themeNames, ', ');
+			  } else {
+			    $content[$i]['themes'] = 'N/A';
+			  }
+			}
+			if (!empty($content2['data']['demographic'])) {
+			  $demographics = $content2['data']['demographic'];
+			  $demographicNames = '';
+			  if (is_array($demographics)) {
+			  foreach ($demographics as $demographic) {
+				$demographicNames .= $demographic['name'] . ', ';
+			  }
+			  }
+			  $content[$i]['demographic'] = rtrim($demographicNames, ', ');
+			} else {
+			  $content[$i]['demographic'] = 'N/A';
+			}
+			if (!empty($content[$i]['anime_title'])) {
+			  $content[$i]['anime_title'] = str_replace(['"', '[', ']'], '', $content[$i]['anime_title']);
+			} else {
+			  $content[$i]['manga_title'] = str_replace(['"', '[', ']'], '', $content[$i]['manga_title']);
+			}
+			if (!empty($content[$i]['anime_title_eng'])) {
+			  $content[$i]['anime_title_eng'] = str_replace(['"', '[', ']'], '', $content[$i]['anime_title_eng']);
+			} else {
+			  $content[$i]['manga_english'] = str_replace(['"', '[', ']'], '', $content[$i]['manga_english']);
+			}
+			if (!empty($content[$i]['anime_id'])) {
+			  if (isset($content2['data']['title_german']) && $content2['data']['title_german'] !== null) {
+			    $content[$i]['anime_title_de'] = str_replace(['"', '[', ']'], '', $content2['data']['title_german']);
+			  } else {
+				if ($content[$i]['anime_title_eng'] !== 'N/A') {
+			      $content[$i]['anime_title_de'] = $content[$i]['anime_title_eng'];
+				} else {
+				  $content[$i]['anime_title_de'] = $content[$i]['anime_title'];
+				}
+			  }
+			} else {
+			  if (isset($content2['data']['title_german']) && $content2['data']['title_german'] !== null) {
+			    $content[$i]['manga_title_de'] = str_replace(['"', '[', ']'], '', $content2['data']['title_german']);
+			  } else {
+			    if ($content[$i]['manga_english'] !== 'N/A') {
+			      $content[$i]['manga_title_de'] = $content[$i]['manga_english'];
+				} else {
+				  $content[$i]['manga_title_de'] = $content[$i]['manga_title'];
+				}
+			  }
+			}
+			if (!empty($content[$i]['anime_id'])) {
+			  if (isset($content2['data']['title_japanese']) && $content2['data']['title_japanese'] !== null) {
+			    $content[$i]['anime_title_jp'] = str_replace(['"', '[', ']'], '', $content2['data']['title_japanese']);
+			  } else {
+				if ($content[$i]['anime_title_eng'] !== 'N/A') {
+			      $content[$i]['anime_title_jp'] = $content[$i]['anime_title_eng'];
+				} else {
+				  $content[$i]['anime_title_jp'] = $content[$i]['anime_title'];
+				}
+			  }
+			} else {
+			  if (isset($content2['data']['title_japanese']) && $content2['data']['title_japanese'] !== null) {
+			    $content[$i]['manga_title_jp'] = str_replace(['"', '[', ']'], '', $content2['data']['title_japanese']);
+			  } else {
+			    if ($content[$i]['manga_english'] !== 'N/A') {
+			      $content[$i]['manga_title_jp'] = $content[$i]['manga_english'];
+				} else {
+				  $content[$i]['manga_title_jp'] = $content[$i]['manga_title'];
+				}
+			  }
+			}
+			if (!empty($content[$i]['anime_id'])) {
+			  if ($content[$i]['anime_title_eng'] == 'N/A') {
+			    $content[$i]['anime_title_eng'] = $content[$i]['anime_title'];
+			  }
+			} else {
+			  if ($content[$i]['manga_english'] == 'N/A') {
+			    $content[$i]['manga_english'] = $content[$i]['manga_title'];
+			  }
+			}
+			if (!empty($content[$i]['num_watched_episodes'])) {
+			  if ($content[$i]['anime_num_episodes'] !== 0) {
+			    $content[$i]['progress_percent'] = round(($content[$i]['num_watched_episodes'] / $content[$i]['anime_num_episodes']) * 100, 2);
+			  } else {
+			    $content[$i]['progress_percent'] = 0;
+			  }
+			} elseif (!empty($content[$i]['num_read_volumes'])) {
+			  if ($content[$i]['manga_num_volumes'] !== 0) {
+			    $content[$i]['progress_percent'] = round(($content[$i]['num_read_volumes'] / $content[$i]['manga_num_volumes']) * 100, 2);
+			  } else {
+			    $content[$i]['progress_percent'] = 0;
+			  }
+			} else {
+			    $content[$i]['progress_percent'] = 0;
+			}
+			if (!empty($content2['data']['rank'])) {
+			  $content[$i]['rank'] = $content2['data']['rank'];
+			} else {
+			  $content[$i]['rank'] = "N/A";
+			}
+			if (!empty($content2['data']['serialization'])) {
+			  $serializations = $content2['data']['serialization'];
+			  $serializationNames = '';
+			  $serializations = !is_array($serializations) ? [] : $serializations;
+			  foreach ($serializations as $serialization) {
+				$serializationNames .= $serialization['name'] . ', ';
+			  }
+			  $content[$i]['serialization'] = rtrim($serializationNames, ', ');
+			} else {
+			  $content[$i]['serialization'] = 'N/A';
+			}
+			if (!empty($content['manga_magazines'])) {
+			  $mangamagazines = $content['manga_magazines'];
+			  $mangamagazineNames = '';
+			  $mangamagazines = !is_array($mangamagazines) ? [] : $mangamagazines;
+			  foreach ($mangamagazines as $mangamagazine) {
+				$mangamagazineNames .= $mangamagazine['name'] . ', ';
+			  error_log($mangamagazineNames . ' ' . $mangamagazine['name']);
+			  }
+			  $content[$i]['manga_magazines'] = rtrim($mangamagazineNames, ', ');
+			} else {
+			  $content[$i]['manga_magazines'] = 'N/A';
+			}
+			if (!empty($content[$i]['anime_image_path'])) {
+			  $content[$i]['anime_image_path'] = Helper::imageUrlCleaner($content[$i]['anime_image_path']);
+			} else {
+			  $content[$i]['manga_image_path'] = Helper::imageUrlCleaner($content[$i]['manga_image_path']);
+			}
+			if (!empty($content[$i]['anime_id'])) {
+			  $content[$i]['anime_image_path'] = Helper::imageUrlReplace($content[$i]['anime_id'], 'anime', $content[$i]['anime_image_path'], $this->_user);
+			} else {
+			  $content[$i]['manga_image_path'] = Helper::imageUrlReplace($content[$i]['manga_id'], 'manga', $content[$i]['manga_image_path'], $this->_user);
+			}
+			if ($content[$i]['status'] == 1) {
+			    $te_cwr += 1;
+				$te_all += 1;
+			} elseif ($content[$i]['status'] == 2) {
+			    $te_c += 1;
+				$te_all += 1;
+			} elseif ($content[$i]['status'] == 3) {
+			    $te_oh += 1;
+				$te_all += 1;
+			} elseif ($content[$i]['status'] == 4) {
+			    $te_d += 1;
+				$te_all += 1;
+			} elseif ($content[$i]['status'] == 6) {
+			    $te_ptwr += 1;
+				$te_all += 1;
+			}
+		  $content[$i]['total_entries_cwr'] = $te_cwr;
+		  $content[$i]['total_entries_c'] = $te_c;
+		  $content[$i]['total_entries_oh'] = $te_oh;
+		  $content[$i]['total_entries_d'] = $te_d;
+		  $content[$i]['total_entries_ptwr'] = $te_ptwr;
+		  $content[$i]['total_entries_all'] = $te_all;
+		  $content[$i]['\a'] = "-a";
 		  }
 
 		  $data = array_merge($data, $content);
 
-          // FIX #2: Only increment the offset if we did NOT use the alternate URL.
-          // This stops the infinite loop.
-          if (!$use_alternate_url) {
-		    $offset += 300;
-          } else {
-            // If we did use the alternate, we must exit after this one successful run.
-            break;
-          }
-
+		  $offset += 300;
 		} else {
 		  break;
 		}
 	  }
 
-      return $data;
+        return $data;
     }
 }
